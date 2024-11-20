acea811bea28656dab6c30c5aeb96942e929ac64
Ironboy <ironboy@users.noreply.github.com>
2024-11-20 08:17:25 +0100

    basic example

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 // backend
 import express from 'express';
+import path from 'path';
 const app = express();
 const port = 4000;
 
@@ -12,6 +13,20 @@ app.get('/api/*', async (req, res) => {
 // Category list: /api/c/kott-chark-och-fagel/palagg?size=30&page=0&sort=
 // [topRated, name-asc,name-desc,price-asc,price-desc,compareprice-asc,compareprice-desc]
 // Product info:  /api/axfood/rest/p/101290312_ST
+
+// log 
+app.use((req, res, next) => {
+  next();
+  // console.log(req.url);
+});
+
+// serve built application (dist folder)
+const distFolder = path.join(import.meta.dirname, '..', 'dist');
+app.use(express.static(distFolder));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(distFolder, 'index.html'));
+});
+
 
 // start server
 app.listen(port, () => console.log('Backend listening on port 4000'));
